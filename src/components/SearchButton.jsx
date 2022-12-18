e0723d18415b81412106225ea70f2a0e7e167740
ugurcsen <trmaster101@gmail.com>
2022-12-18 05:43:02 +0300

    Search icon added to search input.

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchButton({text, onSubmit}) {
     const {query} = useRouter()
@@ -16,6 +18,7 @@ export default function SearchButton({text, onSubmit}) {
             {text !== undefined ? (<h3>{text}</h3>) : ""}
             <form onSubmit={onSubmit}>
                 <input placeholder="Search" defaultValue={value} name="s" className="form-control search-input"/>
+                <FontAwesomeIcon icon={faSearch} />
             </form>
         </div>
     )
